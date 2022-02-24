96d290cc3fa45a812580aade813107fe93aee740
OscarSah <oscar@cybertekschool.com>
2022-02-24 22:24:22 +0300

    Account Summary steps started

---
+++ src/test/java/com/zerobank/pages/AccountSummaryPage.java
@@ -0,0 +1,11 @@
+package com.zerobank.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class AccountSummaryPage extends BasePage{
+    @FindBy(tagName = "h2")
+    public List<WebElement> accountTypes;
+}
